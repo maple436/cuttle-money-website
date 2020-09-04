af15c33a16d657f0e0a4795df1548c139a59be20
maple <2313694690@qq.com>
2020-09-04 15:13:31 +0800

    deploy

---
+++ precache-manifest.ae86b3706cbae504c631c8f9a652158a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ebcd280e9a3116ea8a",
+    "url": "/cuttle-money-website/css/app.b4e14834.css"
+  },
+  {
+    "revision": "59ae3ced5b8ec27da1a3a56fce619a14",
+    "url": "/cuttle-money-website/index.html"
+  },
+  {
+    "revision": "60ebcd280e9a3116ea8a",
+    "url": "/cuttle-money-website/js/app.62a54297.js"
+  },
+  {
+    "revision": "68280e7e9c2043063f85",
+    "url": "/cuttle-money-website/js/chunk-vendors.a3d553be.js"
+  },
+  {
+    "revision": "8d27931576ffd77081a19c569f16e465",
+    "url": "/cuttle-money-website/manifest.json"
+  },
+  {
+    "revision": "cab12d6e5bc380e14651d8f8caf8b1f8",
+    "url": "/cuttle-money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cuttle-money-website/robots.txt"
+  }
+]);
